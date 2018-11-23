e9a7b50a9665bcb825ec3bd94bd26049bf8d8b2b
jlavoine <joe.lavoine@gmail.com>
2018-11-22 19:37:01 -0500

    Adding camera to follow player

---
+++ Assets/Scripts/Camera/FollowPlayerCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FollowPlayerCamera : MonoBehaviour {
+    public GameObject PlayerCharacter;
+
+    private Vector3 _offset;
+
+	void Start () {
+        _offset = transform.position + PlayerCharacter.transform.position;
+	}
+	
+	void LateUpdate () {
+        transform.position = PlayerCharacter.transform.position + _offset;
+	}
+}
